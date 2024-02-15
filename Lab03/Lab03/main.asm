2008c28f09a895c34e1041673745e55e39d7a352
giovannijim <99777062+giovannijim@users.noreply.github.com>
2024-02-15 10:55:16 -0600

    LAB03

--- Lab03/Lab03/main.asm
+++ Lab03/Lab03/main.asm
@@ -11,7 +11,7 @@
 .cseg
 .org 0x00
 	JMP MAIN
-.org 0x0006     // Vector de ISR_INT0
+.org 0x0006     // Vector de ISR_PCINT0
 	JMP ISR_PCINT0 
 .org 0x0020		// Vector de TIMER0_OVF
 	JMP ISR_TIMER0_OVF
@@ -48,8 +48,8 @@ Setup:
 	LDI R16, 0x00
 	OUT PORTC, R16 ; Apagar los pines
 
-	LDI	R16, 0xFF
-	OUT	DDRD, R16
+	LDI	R16, 0xFF // Establece todo el puerto D como salida
+	OUT	DDRD, R16 // carga el registro 16 en puerto DDRD
 
 	LDI R16, (0<<PB4)|(0<<PB3)
 	OUT DDRB, R16  ; Se establecen los pines PB4 Y PB3 como entradas
@@ -74,47 +74,43 @@ Setup:
 	LDI R20, 0x00 ; Contador en 0, BITS, LEDS
 	LDI R21, 0x00 ; Contador en 0, C. DE 1 S
 
-	//LPM R17, Z
-	LDI R17, 0xFC
-	OUT PORTD, R17
+	LDI R17, 0xFC // Carga el digito 0 en los displays
+	OUT PORTD, R17 // Carga el registro hacia el portD
 	
-
 	STS UCSR0B, R18 //Desactiva los puertos TX y RX
 
-	LDI R17, 0x00 ; Contador en 0
+	LDI R17, 0x00 // Contador en 0
 	SBI PORTB, PB1 //decenas
 	CBI PORTB, PB2 //unidades
 //*****************************************************************************
 // LOOP
 //*****************************************************************************
-
 Loop:		
 	OUT PORTC, R20 // Carga el valor de R20 a PortC
 
 	AUMENTO_UNIDADES:
-	//CALL MEGA_DELAY
-	CPI R26, 1 ; Contar si ya llego a 10ms
-	BRNE AUMENTO_UNIDADES
-	CLR R26
+	CPI R26, 1 // Contar si ya llego a 10ms
+	BRNE AUMENTO_UNIDADES // Salta a dicha subrutina (regresa), si no es igual
+	CLR R26 // Limpia el registro 26
 	CBI PORTB, PB1 // Desactiva display de decenas
 	SBI PORTB, PB2 // Activa display de unidades
 	LDI ZL, LOW(TABLE << 1) // Se obtiene la dirección menos significativa del registro Z
 	LDI ZH, HIGH(TABLE << 1) // Se obtiene la dirección más significativa del registro Z
-	ldi r16, 1
-	sub zl, r16
-	ADD ZL, R24
+	LDI R16, 1 //Se carga 1 en el registro 16
+	SUB ZL, R16 // A zl se le resta 1
+	ADD ZL, R24	// Se desplaza el pointer
 	LPM R16, Z // Carga el valor que existe en Z en el registro R 16
 	OUT PORTD, R16 // Carga el registro R16 al puerto D
 
 	AUMENTO_DECENAS:
-	CPI R25, 2 ; Contar si ya llego a 20ms
-	BRNE AUMENTO
-	CLR R25
+	CPI R25, 2 // Contar si ya llego a 20ms
+	BRNE AUMENTO // Salta a dicha subrutina (regresa), si no es igual
+	CLR R25 // limpia el registro 25
 	SBI PORTB, PB1 // Activa display de decenas
 	CBI PORTB, PB2 // Desactiva display de unidades
 	LDI ZL, LOW(TABLE << 1) // Se obtiene la dirección menos significativa del registro Z
 	LDI ZH, HIGH(TABLE << 1) // Se obtiene la dirección más significativa del registro Z
-	ADD ZL, R17
+	ADD ZL, R17 // Mueve ZL a donde se encuentra el pointer (la posición de R17)
 	LPM R16, Z // Carga el valor que existe en Z en el registro R 16
 	OUT PORTD, R16 // Carga el registro R16 al puerto D
 
@@ -133,70 +129,26 @@ Init_T0:
 	OUT TCCR0B, R16 ; CONFIGURA EL PRESCALER A 1024 PARA UN RELOJ DE 2MHZ
     LDI R16, 236 ; CARGA VALOR DE DESBORDAMIENTO
 	OUT TCNT0, R16 ; CARGA EL VALOR INICIAL DEL CONTADOR
-	LDI	R16, (1 << TOIE0)
+	LDI	R16, (1 << TOIE0)	// Habilita la interrupción del overflow
 	STS	TIMSK0, R16
 	RET
 //******************************************************************************
 // SUBRUTINA PARA INICIALIZAR AUMENTAR EL CONTADOR HEXADECIMAL
 //******************************************************************************
-MEGA_DELAY:
-	LDI R16, 255
-	delay3:
-		DEC R16
-		BRNE delay3
-	LDI R16, 255
-	delay4:
-		DEC R16
-		BRNE delay4
-	LDI R16, 255
-	delay5:
-		DEC R16
-		BRNE delay5
-	LDI R16, 255
-	delay6:
-		DEC R16
-		BRNE delay6
-	LDI R16, 255
-	delay7:
-		DEC R16
-		BRNE delay7
-	LDI R16, 255
-	delay8:
-		DEC R16
-		BRNE delay8
-	brne mega_delay
-RET
 AUMENTO_HEX:
-	;CPI R21, 1 ; Compara si ya llego a los 1000 ms
-	;BRNE AUMENTO_HEX ; Sino ha lelgado enviar a Check b1
-	;SBI PINB, PB1 // Desactiva display de decenas
-	;SBI PINB, PB2 // Activa display de unidades
-	;LDI ZL, LOW(TABLE << 1) // Se obtiene la dirección menos significativa del registro Z
-	;LDI ZH, HIGH(TABLE << 1) // Se obtiene la dirección más significativa del registro Z
-	CPI R24, 0x0A // Compara si el contador es 10
-	BREQ RESET_HEX // Si el contador es cero salta al reset
-	;ADD ZL, R24 // Suma ZL y R24
-	;LPM R16, Z // Carga el valor que existe en Z en el registro R 16
-	;OUT PORTD, R16 // Carga el registro R16 al puerto D
-	INC R24 // Incrementa R24
+	CPI R24, 0x0A		// Compara si el contador es 10
+	BREQ RESET_HEX		// Si el contador es cero salta al reset
+	INC R24				// Incrementa R24
 	RET
-RESET_HEX:
-	//CBI PORTB, PB1 // Activa display de decenas
-	//SBI PORTB, PB2  // Desactiva display de unidades
-	;LDI ZL, LOW(TABLE << 1) // Se obtiene la dirección menos significativa del registro Z
-	;LDI ZH, HIGH(TABLE << 1) // Se obtiene la dirección más significativa del registro Z
-	
-	CPI R17, 0x05 // Compara si el contador es 10
-	BREQ RESET_BOTH // Si el contador es cero salta al reset
-	INC R17 // Incrementa Contador de decenas///
-	;ADD ZL, R17
-	;LPM R16, Z // Carga el valor que existe en Z en el registro R 16
-	;OUT PORTD, R16 // Carga el registro R16 al puerto D
-	CLR R24
+RESET_HEX:	
+	CPI R17, 0x05		// Compara si el contador es 10
+	BREQ RESET_BOTH		// Si el contador es cero salta al reset
+	INC R17				// Incrementa Contador de decenas
+	CLR R24				// Limpia el registro 24
 	RJMP AUMENTO_HEX
 RESET_BOTH:
-	CLR R17
-	CLR R24
+	CLR R17				// Limpia el registro 17
+	CLR R24				// Limpia el registro 24
 	RJMP AUMENTO_HEX
 //*****************************************************************************
 // Subrutina de ISR INT0
@@ -207,47 +159,47 @@ ISR_PCINT0:
 	PUSH R16 ; Guardamos en pila el registro SREG
 
 	IN R18, PINB // Obtiene los valores de pinb, se almacenan en el registro R18
-	/*SBRC R18, PB3
-	RJMP CHECKPB3
+	/*
+	Inicio:
+	SBRC R18, PB3
+	RJMP CHECKPB4
 	DEC R20
 	RJMP SALIR
 
-	CHECKPB3:
-	SBRC R18, PB2
+	CHECKPB4:
+	IN R18, PINB 
+	SBRC R18, PB4
 	RJMP SALIR
-
 	INC R20
-	RJMP SALIR*/
+	RJMP SALIR
+	*/
 
-	SBRS R18, PB3 // Revisa si PB3 esta presionado
-	RJMP CHECKPB2 // Si está presionado, se dirige a la otra subrutina
+	SBRS R18, PB3	// Revisa si PB3 esta presionado
+	RJMP CHECKPB2	// Si está presionado, se dirige a la otra subrutina
 	
-	SBRS R18, PB4 // Revisa si PB4 esta presionado
-	RJMP CHECKPB1  // Si está presionado, se dirige a la otra subrutina
+	SBRS R18, PB4	// Revisa si PB4 esta presionado
+	RJMP CHECKPB1	// Si está presionado, se dirige a la otra subrutina
 	
-	JMP SALIR // Realiza un salto a la subrutina de salida
+	JMP SALIR		// Realiza un salto a la subrutina de salida
 
 CHECKPB1:
-	/*LDI R19, 255
-	delay:
-		DEC R19
-		BRNE delay*/
-	DEC R20 // Decrementa 1 en el valor del registro R20
-	CPI R20, 0x00 // Compara si ya llego a 0 el registro 20
-	BRNE SALIR // Si no son iguales se dirige a la subrutina de salida
-	
-	//LDI R20, 0x00 // Carga el valor 0 en R20 en caso sean iguales
-
-CHECKPB2:
-	/*LDI R19, 255
+	LDI R19, 255
 	delay1:
 		DEC R19
-		BRNE delay1*/
-	INC R20 // Incrementa 1 en el valor del registro R20
-	CPI R20, 0x10 // Compara si ya llego a 16 el registro 20
-	BRNE SALIR  // Si no son iguales se dirige a la subrutina de salida
-	LDI R20, 0x0F // Carga el valor 00 en R20 en caso sean iguales
-	
+		BRNE delay1
+	DEC R20			// Decrementa 1 en el valor del registro R20
+	BRNE SALIR		// Si no son iguales se dirige a la subrutina de salida
+
+CHECKPB2:
+	LDI R19, 255
+	delay2:
+		DEC R19
+		BRNE delay2
+	INC R20				// Incrementa 1 en el valor del registro R20
+	CPI R20, 0x10		// Compara si ya llego a 16 el registro 20
+	BRNE SALIR			// Si no son iguales se dirige a la subrutina de salida
+	LDI R20, 0x0F		// Carga el valor 00 en R20 en caso sean iguales
+
 SALIR:
 	SBI PINB, PB5 ; Toggle Pb5
 	SBI PCIFR, PCIF0 ; Apagar la bandera de ISR PCINT0
@@ -256,7 +208,6 @@ SALIR:
 	OUT SREG, R16		; REstaurar los antiguos vlaores de SREG
 	POP R16				; OBTENER EL VALOR DE r16
 	RETI				; Retornamos a la ISR
-
 //*****************************************************************************
 // Subrutina de ISR_TIMER0_OVF
 //*****************************************************************************
