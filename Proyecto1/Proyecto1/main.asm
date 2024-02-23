91ae665daaedba86cd2549fd56fd82448fa07272
giovannijim <99777062+giovannijim@users.noreply.github.com>
2024-02-23 11:04:34 -0600

    Avance proyecto 1

--- Proyecto1/Proyecto1/main.asm
+++ Proyecto1/Proyecto1/main.asm
@@ -11,6 +11,15 @@
 .cseg
 .org 0x00
 	JMP MAIN
+
+.def CntSegUnidades = R18
+.def CntSegDecenas = R19
+.def CntMinUnidades = R20
+.def CntMinDecenas = R21
+.def CntHrsUnidades = R22
+.def CntHrsDecenas = R23
+.def HRS = R24
+
 .org 0x0006     // Vector de ISR_PCINT0
 	JMP ISR_PCINT0 
 .org 0x0020		// Vector de TIMER0_OVF
@@ -60,3 +69,101 @@ MAIN:
 	OUT DDRB, R16			// Se establece PB5 como salida
 	LDI R16, 0x00
 	OUT PORTC, R16			// Se limpia el puerto B, para que se encuentre en 0
+
+	CLR CntSegDecenas			// Limpia el registro 
+	CLR CntSegUnidades			// Limpia el registro 
+	CLR CntMinUnidades
+	CLR CntMinDecenas
+//*****************************************************************************
+// LOOP
+//*****************************************************************************
+LOOP:	
+
+RJMP LOOP
+//******************************************************************************
+// SUBRUTINA PARA INICIALIZAR TIMER 0
+//******************************************************************************
+Init_T0:
+	LDI R16, (1<< CS02)|(1<<CS00) ; Carga el siguiente byte a R16 0x04
+	OUT TCCR0B, R16 ; CONFIGURA EL PRESCALER A 1024 PARA UN RELOJ DE 2MHZ
+    LDI R16, 236 ; CARGA VALOR DE DESBORDAMIENTO
+	OUT TCNT0, R16 ; CARGA EL VALOR INICIAL DEL CONTADOR
+	LDI	R16, (1 << TOIE0)	// Habilita la interrupción del overflow
+	STS	TIMSK0, R16
+	RET
+//******************************************************************************
+// SUBRUTINA PARA INICIALIZAR AUMENTAR EL CONTADOR HEXADECIMAL
+//******************************************************************************
+AUMENTO_UNIDADES_SEGUNDOS:
+	CPI CntSegUnidades, 0x0A		// Compara si el contador es 10
+	BREQ AUMENTO_DECENAS_SEGUNDOS		// Si el contador es cero salta 
+	INC CntSegUnidades				// Incrementa 
+	RET
+AUMENTO_DECENAS_SEGUNDOS:
+	CPI CntSegDecenas, 0x05		// Compara si el contador de decenas es 5
+	BREQ AUMENTO_UNIDADES_MINUTOS		// Si el contador es cero salta 
+	INC CntSegDecenas				// Incrementa Contador de decenas
+	CLR CntSegUnidades				// Limpia el registro 
+	RJMP AUMENTO_UNIDADES_SEGUNDOS
+AUMENTO_UNIDADES_MINUTOS:
+	CLR CntSegDecenas			// Limpia el registro 
+	CLR CntSegUnidades			// Limpia el registro 
+	INC CntMinUnidades
+	CPI CntMinUnidades, 0x0A		// Compara si el contador de unidades es 9
+	BREQ AUMENTO_DECENAS_MINUTOS		// Si el contador es cero salta al reset
+	RJMP AUMENTO_UNIDADES_SEGUNDOS
+AUMENTO_DECENAS_MINUTOS:
+	CLR CntMinUnidades			// Limpia el registro 
+	INC CntMinDecenas
+	CPI CntMinDecenas, 0x05		// Compara si el contador de decenas es 5
+	BREQ AUMENTO_UNIDADES_HORAS	// Si el contador es cero salta al reset
+	RJMP AUMENTO_UNIDADES_SEGUNDOS
+AUMENTO_UNIDADES_HORAS:
+	CLR CntMinDecenas			// Limpia el registro 
+	CLR CntMinUnidades			// Limpia el registro
+	INC HRS 
+	CPI HRS, 24
+	BREQ RESET_ALL
+	INC CntHrsUnidades
+	CPI CntHrsUnidades, 0x0A		// Compara si el contador de unidades es 9
+	BREQ AUMENTO_DECENAS_HORAS	// Si el contador es cero salta al reset
+	RJMP AUMENTO_UNIDADES_SEGUNDOS
+AUMENTO_DECENAS_HORAS:
+	CLR CntHrsUnidades
+	INC CntHrsDecenas
+	RJMP AUMENTO_UNIDADES_SEGUNDOS
+RESET_ALL:
+	CLR CntSegUnidades			// Limpia el registro 
+	CLR CntSegDecenas			// Limpia el registro 
+	CLR CntMinUnidades			// Limpia el registro
+	CLR CntMinDecenas			// Limpia el registro 
+	CLR CntHrsUnidades
+	CLR CntHrsDecenas
+	CLR HRS
+	RJMP AUMENTO_UNIDADES_SEGUNDOS
+//*****************************************************************************
+// Subrutina de ISR_TIMER0_OVF
+//*****************************************************************************
+ISR_TIMER0_OVF:
+	PUSH R17 ; Guardamos en pila el registro R16
+	IN R17, SREG
+	PUSH R17 ; Guardamos en pila el registro SREG
+
+	LDI R16, 236		; Cargar el valor de desbordamiento
+	OUT TCNT0, R16		; Cargar el valor inicial al contador
+	SBI TIFR0, TOV0		; Borramos la bandera de TOV0
+	INC R27 			; Incrementamos contador de 10 ms
+	INC R28				; Incrementamos contador para toggle de display cada 20ms
+	INC R29				; Incrementamos contador para toggle de display cada 10ms
+
+SALIR2:
+	POP R17				; Obtener el valor de SREG
+	OUT SREG, R17		; REstaurar los antiguos vlaores de SREG
+	POP R17				; OBTENER EL VALOR DE r16
+	RETI				; Retornamos a la ISR
+//******************************************************************************
+//Tabla de valores
+//******************************************************************************
+	TABLADOS: .DB 0x20, 0x10
+	TABLE: .DB  0xFC, 0xC0, 0x6E, 0xEA, 0xD2, 0xBA, 0xBE, 0xE2, 0xFE, 0xF2, 0xF6, 0x9E, 0x3C, 0xCE, 0x3E, 0x36
+//******************************************************************************
