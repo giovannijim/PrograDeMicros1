633e129d37a6cf142d1eeb647af44f3ea5342734
giovannijim <99777062+giovannijim@users.noreply.github.com>
2024-04-04 11:27:15 -0600

    prelab04

---
+++ Prelab04/Prelab04/main.c
@@ -0,0 +1,18 @@
+/*
+ * Prelab04.c
+ *
+ * Created: 4/4/2024 10:05:52 AM
+ * Author : GIOLESS
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
