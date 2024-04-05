f59111eb22db98557aad6108f4efe3c1061a930f
giovannijim <99777062+giovannijim@users.noreply.github.com>
2024-04-04 20:42:07 -0600

    Update main.c

--- Prelab04/Prelab04/main.c
+++ Prelab04/Prelab04/main.c
@@ -4,13 +4,21 @@
  * Created: 4/4/2024 10:05:52 AM
  * Author : GIOLESS
  */ 
-
+#define F_CPU = 16000000
 #include <avr/io.h>
 
 
 int main(void)
 {
-    /* Replace with your application code */
+    DDRD |= 0xFF;
+    PORTD = 0;
+    DDRC |= 0xFF;
+    PORTC = 0;
+    DDRB |= 0xFF;
+    PORTB = 0;
+
+    DDRB &= ~(1<<PB1)
+    DDRB &= ~(1<<PB0)
     while (1) 
     {
     }
