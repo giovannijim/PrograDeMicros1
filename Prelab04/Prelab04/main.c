2a624a8131c599e53948a3daf7ca06f803d88de8
giovannijim <99777062+giovannijim@users.noreply.github.com>
2024-04-05 10:13:48 -0600

    Funciona Contador Solito

--- Prelab04/Prelab04/main.c
+++ Prelab04/Prelab04/main.c
@@ -4,23 +4,47 @@
  * Created: 4/4/2024 10:05:52 AM
  * Author : GIOLESS
  */ 
-#define F_CPU = 16000000
+#define F_CPU 16000000
 #include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
 
+void setup(void);
+
+char contador;
 
 int main(void)
 {
-    DDRD |= 0xFF;
-    PORTD = 0;
-    DDRC |= 0xFF;
-    PORTC = 0;
-    DDRB |= 0xFF;
-    PORTB = 0;
-
-    DDRB &= ~(1<<PB1)
-    DDRB &= ~(1<<PB0)
-    while (1) 
-    {
-    }
+	cli();
+	setup();
+	sei();
+	while(1)
+	{
+		contador ++;
+		PORTD = contador;
+		_delay_ms(200);
+	}
 }
 
+void setup(void)
+{
+	contador = 0x00;
+	// Establecer como salidas puerto B, C y D
+	UCSR0B = 0;
+	DDRD |= 0xFF;
+	PORTD = 0;
+	
+	//Establecer el puerto C como entrada
+	DDRC = 0;
+	
+	// Establecer con pull up el puerto PC5 Y PC4
+	PORTC |= (1<<PORTC4);
+	PORTC |= (1<<PORTC5);
+	
+	
+}
+
+ISR (PCINT1_vect) 
+{
+	
+}
