5488b8ea76b27db58e16119f7a36cbb5f8c6768a
giovannijim <99777062+giovannijim@users.noreply.github.com>
2024-05-10 18:26:24 -0600

    memorria eeprom

--- Proyecto2/Proyecto2/main.c
+++ Proyecto2/Proyecto2/main.c
@@ -15,10 +15,19 @@
 #include "PWM2/PWM2.h"
 #include "PWM1/PWM1.h"
 #include "UART/UART.h"
+#include <avr/eeprom.h>
 
 void setup(void);
 
 volatile uint8_t  bufferRX;
+uint8_t estado;
+uint8_t position;
+
+uint8_t memoria1;
+uint8_t memoria2;
+uint8_t memoria3;
+uint8_t memoria4;
+
 
 int main(void)
 {
@@ -33,12 +42,14 @@ int main(void)
 	
     while (1) 
     {
+		// Modo MANUAL
+		if (estado == 0){
+			PORTB &= ~(1<<PORTB5);
  		//inicializar ADC7
  		initADC(7);
  		ADCSRA |= (1<< ADSC);				// Comenzar conversion
  		while(ADCSRA&(1<<ADSC));			// Revisar si la conversion ya termino
  		updateDutyCyclePWM2A(ADCH);			// Se llama la función de la librería
-		
 
  		//inicializar ADC6
  		initADC(6);
@@ -57,17 +68,36 @@ int main(void)
  		ADCSRA |= (1<< ADSC);				// Comenzar conversion
  		while(ADCSRA&(1<<ADSC));			// Revisar si la conversion ya termino
  		updateDutyCyclePWM0B(ADCH);			// Se llama la función de la librería
+		}
+		
+		// Modo MEMORIA
+		else if ( estado == 1)
+		{
+			PORTB |= (1<<PORTB5);
+			memoria1 =  eeprom_read_byte((uint8_t*)(0+(4*position)));
+			memoria2 =  eeprom_read_byte((uint8_t*)(1+(4*position)));
+			memoria3 =  eeprom_read_byte((uint8_t*)(2+(4*position)));
+			memoria4 =  eeprom_read_byte((uint8_t*)(3+(4*position)));
+			updateDutyCyclePWM2A(memoria1);			// Actualizar el DutyCycle
+			updateDutyCyclePWM2B(memoria2);			// Actualizar el DutyCycle
+			updateDutyCyclePWM0A(memoria3);			// Actualizar el DutyCycle
+			updateDutyCyclePWM0B(memoria4);			// Actualizar el DutyCycle
+		}	
+		
     }
 }
 
 void setup(void)
 {
+	estado = 0;
+	position = 0;
+	
 	//ESTABLECER PUERTO C1, C2 Y C3 COMO ENTRADA
 	DDRC &= ~(1<<PORTC1)|~(1<<PORTC2)|~(1<<PORTC3);
 	//Habilitar la interrupción puerto C
 	PCICR |= (1<<PCIE1);
 	// Habilitar mascara para pines PC1 PC2, PC3
-	PCMSK1 |= 0b00001110;
+	PCMSK1 = 0b00001110;
 	
 }
 
@@ -88,10 +118,42 @@ ISR(USART_RX_vect)
 	
 ISR(PCINT1_vect)
 {
-	if(!(PINC&(1<<PINC2))) // Si PINC2 se encuentra apagado ejecutar instrucción
+	if(!(PINC&(1<<PINC3))) // Si PINC3 se encuentra apagado ejecutar instrucción
 	{
-		PORTB |= (1<<PORTB5);
-		Menu("LED2\n");
+		if(estado < 2){estado ++;}
+			else{estado=0;}
 	}
+	else if(!(PINC&(1<<PINC2))) // Si PINC3 se encuentra apagado ejecutar instrucción
+	{
+		if(position < 3){position ++;}
+			else{position=0;}
+	}
+	else if(!(PINC&(1<<PINC1))) // Si PINC3 se encuentra apagado ejecutar instrucción
+	{
+		//inicializar ADC7
+		initADC(7);
+		ADCSRA |= (1<< ADSC);				// Comenzar conversion
+		while(ADCSRA&(1<<ADSC));			// Revisar si la conversion ya termino
+		eeprom_write_byte((unsigned char*)(0+(position*4)), ADCH);
+
+		//inicializar ADC6
+		initADC(6);
+		ADCSRA |= (1<< ADSC);				// Comenzar conversion
+		while(ADCSRA&(1<<ADSC));			// Revisar si la conversion ya termino
+		eeprom_write_byte((unsigned char*)(1+(position*4)), ADCH);
+		
+		//inicializar ADC5
+		initADC(5);
+		ADCSRA |= (1<< ADSC);				// Comenzar conversion
+		while(ADCSRA&(1<<ADSC));			// Revisar si la conversion ya termino
+		eeprom_write_byte((unsigned char*)(2+(position*4)), ADCH);
+		
+		//inicializar ADC4
+		initADC(4);
+		ADCSRA |= (1<< ADSC);				// Comenzar conversion
+		while(ADCSRA&(1<<ADSC));			// Revisar si la conversion ya termino
+		eeprom_write_byte((unsigned char*)(3+(position*4)), ADCH);
+	}
+	
 	PCIFR |= (1<<PCIF1); // Apagar la bandera de interrupción
 }	
